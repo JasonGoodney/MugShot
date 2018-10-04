0389523212d9dd1ed64b3c3e7982ee1e12cb438b
Jason Goodney <jason.goodney@gmail.com>
2018-10-04 13:12:13 -0600

    adds viewcontrollers and anchor extension

---
+++ MugShot/Source/ViewController/EndGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EndGameViewController.swift
+//  MugShot
+//
+//  Created by Jason Goodney on 10/4/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import UIKit
+
+class EndGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
---
+++ MugShot/Source/ViewController/PlayGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlayGameViewController.swift
+//  MugShot
+//
+//  Created by Jason Goodney on 10/4/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import UIKit
+
+class PlayGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+}
