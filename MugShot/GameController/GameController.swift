6fe857b5683e4a336bab6665e839afc9077b6d5f
Kamil Wrobel <wrobelk@live.com>
2018-10-04 19:34:57 -0600

    creates an array of enemies

--- MugShot/GameController/GameController.swift
+++ MugShot/GameController/GameController.swift
@@ -24,4 +24,25 @@ class GameController {
     
     
     
+    
+    
+    
+   var enemies : [Enemy]  {
+        let kamil = Enemy.init(image: #imageLiteral(resourceName: "kamil"), tapAmout: 1, speed: 1, name: "Kamil")
+        let jason = Enemy.init(image: #imageLiteral(resourceName: "jason"), tapAmout: 1, speed: 1, name: "Jason")
+        let jayden = Enemy.init(image: #imageLiteral(resourceName: "jayden"), tapAmout: 2, speed: 1, name: "Jayden")
+        let nick = Enemy.init(image: #imageLiteral(resourceName: "nick"), tapAmout: 3, speed: 1, name: "Nick")
+        let john = Enemy.init(image: #imageLiteral(resourceName: "john"), tapAmout: 1, speed: 1, name: "John")
+        let abdi = Enemy.init(image: #imageLiteral(resourceName: "abdi"), tapAmout: 1, speed: 1, name: "Abdi")
+        let markus = Enemy.init(image: #imageLiteral(resourceName: "markus"), tapAmout: 1, speed: 1, name: "Markus")
+        let eric = Enemy.init(image: #imageLiteral(resourceName: "eric"), tapAmout: 1, speed: 1, name: "Eric")
+        let cody = Enemy.init(image: #imageLiteral(resourceName: "cody"), tapAmout: 1, speed: 1, name: "Cody")
+        let quinten = Enemy.init(image: #imageLiteral(resourceName: "quinten"), tapAmout: 1, speed: 1, name: "Quinten")
+        let trevor = Enemy.init(image: #imageLiteral(resourceName: "brick"), tapAmout: 3, speed: 1, name: "Trevor")
+        
+        return [kamil, jason, jayden, nick, john, abdi, markus, eric, cody, quinten, trevor]
+        
+    }
+    
+
 }
--- MugShot/Model/Enemy.swift
+++ MugShot/Model/Enemy.swift
@@ -16,3 +16,6 @@ struct Enemy {
     var name:  String
     
 }
+
+
+
